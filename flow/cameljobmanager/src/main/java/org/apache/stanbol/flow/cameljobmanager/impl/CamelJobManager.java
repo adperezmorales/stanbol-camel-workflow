d3af43acc5fc5e21ddc71b5ac42d2aa36201fc42
Antonio David Perez Morales <adperezmorales@gmail.com>
2014-06-09 18:05:19 +0200

    Adjusting export package and added new method to add routes with InputStream containing Camel XML routes

--- flow/cameljobmanager/src/main/java/org/apache/stanbol/flow/cameljobmanager/impl/CamelJobManager.java
+++ flow/cameljobmanager/src/main/java/org/apache/stanbol/flow/cameljobmanager/impl/CamelJobManager.java
@@ -1,7 +1,7 @@
 package org.apache.stanbol.flow.cameljobmanager.impl;
 
 import java.io.IOException;
-import java.util.LinkedList;
+import java.io.InputStream;
 import java.util.List;
 
 import org.apache.camel.CamelContext;
@@ -11,6 +11,7 @@ import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.core.osgi.OsgiDefaultCamelContext;
 import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.model.RoutesDefinition;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Reference;
@@ -30,58 +31,76 @@ import org.osgi.service.component.ComponentContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * Naive EnhancementJobManager implementation that keeps its request queue in
  * memory.
- *
+ * 
  */
-@org.apache.felix.scr.annotations.Component(immediate=true)
+@org.apache.felix.scr.annotations.Component(immediate = true)
 @Service(FlowJobManager.class)
 @References({
-		@Reference(referenceInterface=Component.class, name="Ec", policy=ReferencePolicy.DYNAMIC),
-		@Reference(referenceInterface=RoutesBuilder.class, name="Route", cardinality=ReferenceCardinality.OPTIONAL_MULTIPLE, policy=ReferencePolicy.DYNAMIC)
-		})
+		@Reference(referenceInterface = Component.class, name = "Ec", policy = ReferencePolicy.DYNAMIC),
+		@Reference(referenceInterface = RoutesBuilder.class, name = "Route", cardinality = ReferenceCardinality.OPTIONAL_MULTIPLE, policy = ReferencePolicy.DYNAMIC) })
 public class CamelJobManager implements FlowJobManager {
-	
+
 	EngineComponent ec;
-	
-	private static final Logger LOG = LoggerFactory.getLogger(CamelJobManager.class);
+
+	private static final Logger LOG = LoggerFactory
+			.getLogger(CamelJobManager.class);
 	private CamelContext cContext = null;
-	
+
 	@Reference
-    protected ChainManager chainManager;
-	
+	protected ChainManager chainManager;
+
 	protected void bindRoute(RoutesBuilder e) throws Exception {
-		RouteBuilder srb = (RouteBuilder)e;
-			addRouteToContext(srb);
-    }
-	
+		RouteBuilder srb = (RouteBuilder) e;
+		addRouteToContext(srb);
+	}
+
+	public Boolean addRoutes(InputStream is) {
+		try {
+			RoutesDefinition rds = cContext
+
+			.loadRoutesDefinition(is);
+
+			cContext.addRouteDefinitions(rds.getRoutes());
+			for (RouteDefinition rd : rds.getRoutes()) {
+				cContext.startRoute(rd.getId());
+			}
+
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
 	/**
 	 * Remove route for the Camel context
-	 * @param e : The route builder
-	 * @throws Exception 
+	 * 
+	 * @param e
+	 *            : The route builder
+	 * @throws Exception
 	 */
 	protected void unbindRoute(RoutesBuilder e) throws Exception {
-		RouteBuilder srb = (RouteBuilder)e;
-		for (RouteDefinition routeDefs : srb.getRouteCollection().getRoutes()){
+		RouteBuilder srb = (RouteBuilder) e;
+		for (RouteDefinition routeDefs : srb.getRouteCollection().getRoutes()) {
 			cContext.stopRoute(routeDefs);
 			cContext.removeRouteDefinition(routeDefs);
 		}
 	}
-	
-	protected void bindEc(Component e) {
-		ec= (EngineComponent)e;
-    }
 
-    protected void unbindEc(Component e) {
-        ec = null;
-    }
-    
+	protected void bindEc(Component e) {
+		ec = (EngineComponent) e;
+	}
+
+	protected void unbindEc(Component e) {
+		ec = null;
+	}
+
 	@Activate
-    public void activate(ComponentContext ce) throws IOException {
+	public void activate(ComponentContext ce) throws IOException {
 		cContext = new OsgiDefaultCamelContext(ce.getBundleContext());
-		
+
 		try {
 			cContext.addComponent("engine", ec);
 			cContext.start();
@@ -89,39 +108,42 @@ public class CamelJobManager implements FlowJobManager {
 			throw new IOException(e);
 		}
 	}
-	
+
 	@Deactivate
-    public void deactivate(ComponentContext ce) throws Exception {
-    	cContext.stop();
-    }
-	
+	public void deactivate(ComponentContext ce) throws Exception {
+		cContext.stop();
+	}
+
 	@Override
 	public void enhanceContent(ContentItem ci) throws EnhancementException {
 		Chain defaultChain = chainManager.getDefault();
-        if(defaultChain == null){
-            throw new ChainException("Unable to enhance ContentItem '"+ci.getUri()+
-                "' because currently no enhancement chain is active. Please" +
-                "configure a Chain or enable the default chain");
-        }
-        enhanceContent(ci, defaultChain);
+		if (defaultChain == null) {
+			throw new ChainException(
+					"Unable to enhance ContentItem '"
+							+ ci.getUri()
+							+ "' because currently no enhancement chain is active. Please"
+							+ "configure a Chain or enable the default chain");
+		}
+		enhanceContent(ci, defaultChain);
 	}
-    
-    @Override
-	public void enhanceContent(ContentItem ci, Chain chain) throws EnhancementException {
-    	//TODO : better integration with REST :
-		//http://camel.apache.org/cxfrs.html
-    	ProducerTemplate tpl = cContext.createProducerTemplate();
-		ContentItem result = tpl.requestBody("direct://"+chain.getName(), ci, ContentItem.class);
-    }
-    
+
+	@Override
+	public void enhanceContent(ContentItem ci, Chain chain)
+			throws EnhancementException {
+		// TODO : better integration with REST :
+		// http://camel.apache.org/cxfrs.html
+		ProducerTemplate tpl = cContext.createProducerTemplate();
+		ContentItem result = tpl.requestBody("direct://" + chain.getName(), ci,
+				ContentItem.class);
+	}
 
 	public List<EnhancementEngine> getActiveEngines() {
 		return ec.getEnhancementEngines();
 	}
-	
+
 	private void addRouteToContext(RouteBuilder rb) throws Exception {
 		cContext.addRoutes(rb);
-		for (RouteDefinition rd : rb.getRouteCollection().getRoutes() ){
+		for (RouteDefinition rd : rb.getRouteCollection().getRoutes()) {
 			cContext.startRoute(rd.getId());
 		}
 	}
